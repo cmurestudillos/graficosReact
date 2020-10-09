e0e1c1e9e1c304abb8c58bf17efa65d3af829add
carlos mur <cmurestudillos@gmail.com>
2020-10-09 16:00:50 +0200

    commit

--- src/components/linea/LineaComponent.js
+++ src/components/linea/LineaComponent.js
@@ -1,6 +1,50 @@
 import React, {Component} from 'react';
+import { Line } from 'react-chartjs-2';
+import Button from 'react-bootstrap/Button';
+// Configuracion de la Grafica
+import data from './ConfiguracionLinea';
 
 class LineaComponent extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            datos: data
+        }    
+    }
+    //----------------------------------------------------------------------//
+    // Antes de renderizar, cargar el metodo                                //
+    //----------------------------------------------------------------------//
+    componentDidMount (){  
+        // Log de seguimiento
+        console.log("LineaComponent.js - Metodo componentDidMount");  
+    }    
+    //----------------------------------------------------------------------//
+    // Metodo para generar datos aleatorios en la grafica                   //
+    //----------------------------------------------------------------------//
+    randomize = () => {
+        // Log de seguimiento
+        console.log("LineaComponent.js - Metodo randomize");
+
+        var datosGrafica = this.state.datos;
+
+        for (var i = 0; i < datosGrafica.datasets.length; i++) {
+            for (let j = 0; j < datosGrafica.datasets[i].data.length; j++) {
+                datosGrafica.datasets[i].data[j] = this.generateNumber(i);
+            }
+        }
+
+        this.setState({
+            datos: datosGrafica
+        })
+    };   
+    //----------------------------------------------------------------------//
+    // Metodo para generar los numeros                                      //
+    //----------------------------------------------------------------------//
+    generateNumber = (i) => {
+        return Math.floor((Math.random() * (i < 2 ? 100 : 1000)) + 1);
+    };  
+
     render(){
         // Log de seguimiento
         console.log('LineaComponent.js - Metodo render()');
@@ -9,9 +53,13 @@ class LineaComponent extends Component{
             <div className="container">
                 <h3 className="m-3">Grafico de Lineas</h3>
                 <hr></hr>
+                <Line data={this.state.datos} options={data.options} />
+                <Button className="btn btn-4 btn-block mx-auto w-25" onClick={this.randomize}>Aleatorio</Button>
             </div>
         )
+        
     }
+
 }
 
 export default LineaComponent;
